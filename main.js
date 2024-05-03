aaff12ce8cda9bad441e393c476079ad3f5bc8bb
younusawan91 <realkomalkhan0348@gmail.com>
2024-05-03 18:54:56 +0500

    Adding a Currency Converter Project

---
+++ main.js
@@ -0,0 +1,43 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.green.bold("\n \t Welcome to 'Younus Awan' - Currency Converter\n"));
+// Define the lists of Currencies and thier exchange rate
+let exchange_rate = {
+    "USD": 1,
+    "EUR": 0.9,
+    "JYP": 113,
+    "CAD": 1.3,
+    "AUD": 1.65,
+    "PKR": 277.70,
+    // Add more currencies and their exchange rates here
+};
+// Promt the user to select currencies to convert from and to
+let user_answer = await inquirer.prompt([
+    {
+        name: "from_currency",
+        type: "list",
+        message: "Select the currency to convert from:",
+        choices: ["USD", "EUR", "JYP", "CAD", "AUD", "PKR"]
+    },
+    {
+        name: "to_currency",
+        type: "list",
+        message: "Select the currency to convert into:",
+        choices: ["USD", "EUR", "JYP", "CAD", "AUD", "PKR"]
+    },
+    {
+        name: "amount",
+        type: "input",
+        message: "Enter the amount to convert:"
+    }
+]);
+// Perform currency conversion by using Formula
+let from_amount = exchange_rate[user_answer.from_currency];
+let to_amount = exchange_rate[user_answer.to_currency];
+let amount = user_answer.amount;
+// Formula of conversion
+let base_amount = amount / from_amount;
+let converted_amount = base_amount * to_amount;
+// Display thr converted amount
+console.log(`Converted Amount = ${chalk.magenta.bold(converted_amount.toFixed(2))}`);
